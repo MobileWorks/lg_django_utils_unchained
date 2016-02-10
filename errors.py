45ba4042a4e0c0da1cd8de865d3106041ade32e7
Andy Schriner <andrew@leadgenius.com>
2016-02-09 17:27:36 -0800

    package for dist

---
+++ errors.py
@@ -0,0 +1,36 @@
+import logging
+
+from django.conf import settings
+from raven.contrib.django.raven_compat.models import sentry_exception_handler
+from rest_framework.exceptions import ValidationError, PermissionDenied, \
+    NotAuthenticated, NotFound
+from rest_framework.response import Response
+from rest_framework.views import exception_handler
+
+
+SILENT_DRF_EXCEPTIONS = (
+    ValidationError,
+    PermissionDenied,
+    NotAuthenticated,
+    NotFound
+)
+
+
+def custom_exception_handler(exc, context):
+    """
+    DRF:
+    http://www.django-rest-framework.org/api-guide/exceptions/#custom-exception-handling
+    Sentry/Raven:
+    http://raven.readthedocs.org/en/latest/integrations/django.html#usage
+    """
+    response = exception_handler(exc, context)
+
+    if not isinstance(exc, SILENT_DRF_EXCEPTIONS):
+        sentry_exception_handler(request=context['request']._request)
+
+    logging.exception(exc.message, exc_info=exc)
+
+    # throw the error in dev and testing
+    if response or settings.ENVIRONMENT not in ['production', 'staging']:
+        return response
+    return Response({'error': exc.message}, status=500)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='django_utils_unchained',
+      version='1.0',
+      py_modules=[
+          'errors'
+      ],
+      )
