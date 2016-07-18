6c28fcc1ac13b0da818dbdd5a4bf561413cc74c8
Andy Schriner <andrew@leadgenius.com>
2016-07-18 16:16:08 -0700

    fix missing imports, add test

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # django_utils_unchained
 
 ![](./splash_780.jpg)
+
+
+## Run tests:
+
+```
+testing/scripts/run_tests.sh
+```
--- testing/test_project/settings.py
+++ testing/test_project/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'utils',
     'test_without_migrations',
     'testing.test_app',
